132e11f5f3ea49e0c2e2db5f3e20db2e240878f2
DMilushev <d2@auxcode.com>
2023-04-01 01:44:22 +0300

    add test service

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View, Text } from '../../components/Themed';
+import { AuthService } from '../services/auth-service';
+import { useEffect, useState } from 'react';
 
 export default function TabOneScreen() {
+  const authService =  new AuthService();
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    authService.login({
+      email: '',
+      password: ''
+    }).then(
+      (value) => setText(value.title)
+    );}
+    , []);
   return (
     <View style={styles.container}>
+      <Text>{text}</Text>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
