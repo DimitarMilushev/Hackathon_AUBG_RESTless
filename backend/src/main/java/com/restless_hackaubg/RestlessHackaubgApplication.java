a4c9277708383a59768460d41e886060ac70607b
Vasil Kamburov <tulevskiv@gmail.com>
2023-03-31 21:49:26 +0300

    Add backend structure.

---
+++ backend/src/main/java/com/restless_hackaubg/RestlessHackaubgApplication.java
@@ -0,0 +1,13 @@
+package com.restless_hackaubg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestlessHackaubgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestlessHackaubgApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/restless_hackaubg/RestlessHackaubgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restless_hackaubg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestlessHackaubgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
