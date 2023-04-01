0be19f02ad68fa24787448a3175b2618b18a20ec
Vasil Kamburov <tulevskiv@gmail.com>
2023-04-01 21:25:30 +0300

    Add Search Controller for matching processes.

---
+++ backend/src/main/java/com/restless_hackaubg/controllers/SearchController.java
@@ -0,0 +1,33 @@
+package com.restless_hackaubg.controllers;
+
+import com.restless_hackaubg.models.User;
+import com.restless_hackaubg.models.dtos.RequestMatchDto;
+import com.restless_hackaubg.services.SearchServices;
+import com.restless_hackaubg.utils.Mapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+//@AllArgsConstructor
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    private final SearchServices searchServices;
+    private final Mapper mapper;
+
+    public SearchController(SearchServices searchServices, Mapper mapper) {
+        this.searchServices = searchServices;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public List<User> getMatching(@Valid @RequestBody RequestMatchDto requestMatchDto) {
+        User user = mapper.mapRequestMatchDtoToUser(requestMatchDto);
+        return searchServices.getMatches(user);
+    }
+}
