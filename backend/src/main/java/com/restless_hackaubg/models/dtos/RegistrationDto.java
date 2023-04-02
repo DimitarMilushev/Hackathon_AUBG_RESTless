3caa95f86ed7479481b42d84bfba605e9448f366
Vasil Kamburov <tulevskiv@gmail.com>
2023-04-02 09:06:59 +0300

    Adding the RegistrationDto.

---
+++ backend/src/main/java/com/restless_hackaubg/models/dtos/RegistrationDto.java
@@ -0,0 +1,47 @@
+package com.restless_hackaubg.models.dtos;
+
+public class RegistrationDto {
+
+    private String username;
+
+    private String password;
+
+    private String bio;
+
+    private Integer age;
+
+    public RegistrationDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
